afabc85a726d5544a97b45c85b668e8c2105944e
dyyP50Workstation <douyaoyuan@126.com>
2024-10-17 14:14:16 +0800

    refactor: 空格替换成 tab

--- src/stateMachine.h
+++ src/stateMachine.h
@@ -10,13 +10,13 @@
 
 typedef enum
 {
-    aWait,
-    go,
+	aWait,
+	go,
 } stateMachine_eventResult_t;
 
 typedef enum{
-    released,
-    latched
+	released,
+	latched
 } stateMachineLatch_t;
 
 typedef void (* stateAction)(void *);
@@ -24,35 +24,35 @@ typedef stateMachine_eventResult_t (*eventFunc)(void *);
 
 typedef struct
 {
-    stateAction pEnterAction;
-    stateAction pDoAction;
-    stateAction pExistAction;
+	stateAction pEnterAction;
+	stateAction pDoAction;
+	stateAction pExistAction;
 } stateMachine_actionMap_t;
 
 typedef struct stateMachine_event_s
 {
-    eventFunc pEventForGoing;
-    unsigned int nextState;                     //目标状态
-    struct stateMachine_event_s *nextEvent;     //下一个事件
-} stateMachine_event_t;                         //这是一个单向链表,用于登记多个事件
+	eventFunc pEventForGoing;
+	unsigned int nextState;					//目标状态
+	struct stateMachine_event_s *nextEvent;		//下一个事件
+} stateMachine_event_t;							//这是一个单向链表,用于登记多个事件
 
 typedef struct
 {
-    stateMachineLatch_t latch;              //状态锁，如果是 latched 状态，则状态机运行时，不会检测事件
-    unsigned int stateID_l;                 //状态机的前一个状态
-    unsigned int stateID;                   //当前状态循环的状态
-    stateMachine_actionMap_t actions;       //在本状态时需要执行的动作
-    stateMachine_event_t *events;           //在本状态时，需要进行关注的事件，这是一个数组地址
-    uint64_t roundCounter;                  //这个计数器显示了在本状态期间，状态机轮询的次数
-    void *buffer;                           //一个buffer，用于存放与实际实用场景相关的状态数据
+	stateMachineLatch_t latch;				//状态锁，如果是 latched 状态，则状态机运行时，不会检测事件
+	unsigned int stateID_l;				//状态机的前一个状态
+	unsigned int stateID;					//当前状态循环的状态
+	stateMachine_actionMap_t actions;		//在本状态时需要执行的动作
+	stateMachine_event_t *events;			//在本状态时，需要进行关注的事件，这是一个数组地址
+	uint64_t roundCounter;					//这个计数器显示了在本状态期间，状态机轮询的次数
+	void *buffer;							//一个buffer，用于存放与实际实用场景相关的状态数据
 } stateMachineUnit_t;
 
 typedef struct stateMachine_t_s
 {
-    stateMachineUnit_t *pSMChain;    //存放状态单元的数组空间的地址
-    uint8_t stateID;                //标记当前状态机的状态
-    uint8_t stateID_default;        //状态机的默认状态
-    uint8_t stateIDs_Count;         //状态机的总状态数
+	stateMachineUnit_t *pSMChain;	//存放状态单元的数组空间的地址
+	uint8_t stateID;				//标记当前状态机的状态
+	uint8_t stateID_default;		//状态机的默认状态
+	uint8_t stateIDs_Count;		 //状态机的总状态数
 }stateMachine_t;
 
 //初始化状态表
