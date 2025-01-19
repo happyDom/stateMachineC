38759a3bb9a075f9c35752420adf8033e5118c18
dyyDefault <douyaoyuan@126.com>
2025-01-19 08:38:27 +0800

    feat: 优化模型 memmory 中内存块大小

--- src/memmory.c
+++ src/memmory.c
@@ -1,10 +1,10 @@
 #include "memmory.h"
 #include <stdio.h>
- 
-#define DMEM_BLOCK_SIZE         2      	//内存块大小(x字节)
-#define DMEM_BLOCK_NUM          512     //内存块个数
+
+#define DMEM_BLOCK_SIZE         8		//内存块大小(x字节)
+#define DMEM_BLOCK_NUM          128		//内存块个数
 #define DMEM_TOTAL_SIZE         (DMEM_BLOCK_SIZE*DMEM_BLOCK_NUM)    //内存总大小
- 
+
 typedef enum
 {
     DMEM_FREE   = 0,
