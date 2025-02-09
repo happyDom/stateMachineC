0fa568caecc705f187ddf10360d4815a76363024
dyyDefault <douyaoyuan@126.com>
2025-02-09 22:31:33 +0800

    feat: 内存块大小调整为 4，即一个uint32的大小

--- src/memmory.c
+++ src/memmory.c
@@ -1,7 +1,7 @@
 #include "memmory.h"
 #include <stdio.h>
  
-#define DMEM_BLOCK_SIZE         8      	//内存块大小(x字节)
+#define DMEM_BLOCK_SIZE         4      	//内存块大小(x字节)
 #define DMEM_BLOCK_NUM          128     //内存块个数
 #define DMEM_TOTAL_SIZE         (DMEM_BLOCK_SIZE*DMEM_BLOCK_NUM)    //内存总大小
 
